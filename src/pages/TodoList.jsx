ee76d16ab3fb32259d2b453ae9c3aa665d34546e
fahmidicki <35526570+fahmidicki@users.noreply.github.com>
2020-07-09 20:56:41 +0700

    baru setCompleted menit 12 coba diulang dan pahami

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,23 +7,32 @@ import Todos from "../components/Todos";
 
 const Todolist = () => {
   const [todos, setTodos] = useState([
-    { text: "Belajar satu" },
-    { text: "Learning dua" },
-    { text: "Learning Tiga" }
+    { text: "Belajar satu", isCompleted: false },
+    { text: "Learning dua", isCompleted: false },
+    { text: "Learning Tiga", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = value => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
+    setTodos(addedTodo);
+  };
+
+  const completeTodo = index => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = true;
+
     setTodos(addedTodo);
   };
   const showAddToggle = () => setShowAdd(!showAdd);
+  console.log("todos", todos);
+
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
